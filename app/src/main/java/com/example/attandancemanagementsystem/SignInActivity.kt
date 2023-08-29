2362b8328198f323af3d5a83dbb36cf0dcfe13ca
Tiki09 <beheratiki12@gmail.com>
2023-08-29 22:10:28 +0530

    email checked

--- app/src/main/java/com/example/attandancemanagementsystem/SignInActivity.kt
+++ app/src/main/java/com/example/attandancemanagementsystem/SignInActivity.kt
@@ -1,7 +1,10 @@
 package com.example.attandancemanagementsystem
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.attandancemanagementsystem.databinding.ActivitySignInBinding
 import com.example.attandancemanagementsystem.databinding.ActivitySignupBinding
 
@@ -9,8 +12,24 @@ class SignInActivity : AppCompatActivity() {
     private val loginBinding : ActivitySignInBinding by lazy {
         ActivitySignInBinding.inflate(layoutInflater)
     }
+var MyPref = "myPref"
+    var empId =""
+    var pwd =""
+    lateinit var sharedPreferences : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(loginBinding.root)
+
+        sharedPreferences = getSharedPreferences(MyPref, Context.MODE_PRIVATE)
+        loginBinding.btnSignIn.setOnClickListener {
+            empId = loginBinding.edEmployeeId.text?.trim().toString()
+            pwd = loginBinding.edPwd.text?.trim().toString()
+
+           var editor: SharedPreferences.Editor  = sharedPreferences.edit()
+            editor.putString("EmployeeId",empId)
+            editor.putString("Password",pwd)
+            editor.commit()
+            Toast.makeText(this,"inserted successfully",Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/attandancemanagementsystem/model/RegisterExtra.kt
@@ -0,0 +1,10 @@
+package com.example.attandancemanagementsystem.model
+
+data class RegisterExtra(
+    var email:String,
+    var name:String,
+    var eid:String,
+    var pwd:String,
+    var cnfrmPwd:String,
+    var role:String
+)
